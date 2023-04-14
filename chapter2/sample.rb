40d0c845f049a51f1d2f1a03ef2f5c04e600de78
kesuke390 <ksuke.sakuma@gmail.com>
2023-04-14 10:06:14 +0900

    path

---
+++ chapter2/sample.rb
@@ -0,0 +1,30 @@
+#2-1
+p 2 + 3
+p 2 * 2 * 3.14
+#2-2
+
+p "abc"
+p "abc" + "def"
+
+p "123".to_i +  "456".to_i
+
+#2-3
+coffee = 400
+espresso = 100
+
+p "コーヒー: #{coffee}円"
+p "合計: #{coffee + espresso * 2}円"
+
+#2-6
+coffee = 300
+
+total = coffee * 2
+tax = total * 0.08
+tax_included_price = total + tax
+p tax_included_price.to_i
+
+#2-7
+total = coffee * 2
+p total
+
+#2-6
